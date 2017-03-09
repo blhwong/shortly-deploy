f66307d1149c2d729c739d36be02e3e4f9406bbd
Hack Reactor Students <accounts@hackreactor.com>
2017-03-08 16:10:39 -0800

    Add concat into grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['public/client/*.js'],
+        dest: 'public/built.js', 
+      }
     },
 
     mochaTest: {
@@ -77,6 +84,7 @@ module.exports = function(grunt) {
   ]);
 
   grunt.registerTask('build', [
+    'concat'
   ]);
 
   grunt.registerTask('upload', function(n) {
